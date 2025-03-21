b029a89b39a495437733ae722202ddeaa08a771d
unknown <gmehydra@proton.me>
2025-03-21 12:21:16 -0700

    fix /fy23 page

--- src/app/info-pages/fy23-earnings/fy23-media-sentiment-table/fy23-media-sentiment-table.component.ts
+++ src/app/info-pages/fy23-earnings/fy23-media-sentiment-table/fy23-media-sentiment-table.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { ScreenService } from '../../../shared/services/screen-size.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-fy23-media-sentiment-table',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './fy23-media-sentiment-table.component.html',
   styleUrl: './fy23-media-sentiment-table.component.scss'
 })
 export class Fy23MediaSentimentTableComponent {
+  constructor(private _screenService: ScreenService){
+
+  }
+
+  public get isMobile(): boolean { return this._screenService.isMobile; }
+  public get isLarge(): boolean { return this._screenService.screenWidth > 1100; }
 
 }
