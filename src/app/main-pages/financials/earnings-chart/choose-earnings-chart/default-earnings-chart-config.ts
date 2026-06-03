3f5996594392c42d5f02e63d759bfce9020847c1
unknown <gmehydra@proton.me>
2026-06-03 12:32:12 -0700

    fix mobile earnings bug

--- src/app/main-pages/financials/earnings-chart/choose-earnings-chart/default-earnings-chart-config.ts
+++ src/app/main-pages/financials/earnings-chart/choose-earnings-chart/default-earnings-chart-config.ts
@@ -4,7 +4,7 @@ import { EarningsChartPropertySelection } from "./earnings-chart-property-select
 export const defaultEarningsChartConfig: EarningsChartConfig = {
     period: 'QUARTER',
     startYear: 2018,
-    endYear: 9999,
+    endYear: 2026,
     selectedProperty: EarningsChartPropertySelection.REVENUE_VS_NET_INCOME,
     menuLabel: 'Revenue and Net Income',
 }
